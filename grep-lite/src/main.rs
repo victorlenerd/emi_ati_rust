8ea2575fe173750617c9acde3467b142435c96a8
Nwaokocha Victor <nvonweb@outlook.com>
2023-03-19 23:47:12 -0400

    adds feat to read from file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,5 +1,8 @@
 use regex::Regex;
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() {
     let args = App::new("grep-lite")
@@ -11,26 +14,29 @@ fn main() {
                 .takes_value(true)
                 .required(true)
         )
+        .arg(
+            Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true)
+        )
         .get_matches();
 
 
     let ctx_lines = 2;
     let pattern = args.value_of("pattern").unwrap();
-    let haystack = "\
-Every face, every shop,
-bedroom window, public-house and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+    let input = args.value_of("input").unwrap();
 
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+    let lines = reader.lines();
     let mut tags: Vec<usize> = vec![];
     let mut ctx: Vec<Vec<(usize, String)>> = vec![];
     let re = Regex::new(pattern).unwrap();
 
-    for (i, line) in haystack.lines().enumerate() {
-        let contains_substring =  re.find(line);
+    for (i, line) in lines.enumerate() {
+        let _line = &line.unwrap();
+        let contains_substring =  re.find(_line);
 
         match contains_substring {
             Some(_) => {
@@ -47,13 +53,17 @@ through millions of pages?";
         return;
     }
 
-    for (i, line) in haystack.lines().enumerate() {
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+    let lines = reader.lines();
+    for (i, line) in lines.enumerate() {
+        let _line = &line.unwrap();
         for (j, tag) in tags.iter().enumerate() {
             let lower_bound = tag.saturating_sub(ctx_lines);
             let upper_bound = tag + ctx_lines;
 
             if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(line);
+                let line_as_string = String::from(_line);
                 let local_ctx = (i ,line_as_string);
                 ctx[j].push(local_ctx)
             }
